d3e0719ccc1d3e49dc625c4e458924d2397cf003
MJ <m.javad.aghajanian@gmail.com>
2023-06-16 22:30:51 +0330

    fixed ESList issues

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { User } from './users/user.entity';
 import { Report } from './reports/report.entity';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const cookieSession = require('cookie-session');
 
 @Module({
